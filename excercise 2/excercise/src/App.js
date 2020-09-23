c803142faa423af453389d65e286705d7d681e98
Safstack <69162851+Safstack@users.noreply.github.com>
2020-09-23 21:39:33 +0100

    css edited and h1 element added

--- excercise 2/excercise/src/App.js
+++ excercise 2/excercise/src/App.js
@@ -3,21 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const h1Style = { "fontSize":"30px","color":"#7e2b81",}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1> Welcome to our site </h1>
       </header>
     </div>
   );
